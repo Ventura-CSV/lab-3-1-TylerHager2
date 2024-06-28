a3e2722cf70d15bc3ce7856eb2a193e6da64a9fe
TylerHager2 <tylerhager02@outlook.com>
2024-06-28 06:06:12 -0700

    test 1

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ def main():
     """
     Make your code here
     """
+    
+    if number < 50:
+        range = 1
+    elif 50 <= number < 100:
+        range = 2
+    elif number >= 100:
+        range = 3
 
     print(f'Range is {range}')
     ########################################
